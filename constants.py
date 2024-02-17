96ea172c2da799fbeab8fee8554589ac7eb62d47
Maciej Pleban <maciej.pleban@slsolutions.pl>
2024-02-17 16:53:50 +0100

    initial version of the project

--- constants.py
+++
@@ -1,6 +0,0 @@
-european_countries = ["Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria",
-                      "Croatia", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece",
-                      "Holy See (Vatican City)", "Hungary", "Iceland", "Ireland", "Italy", "Latvia", "Liechtenstein",
-                      "Lithuania", "Luxembourg", "Malta", "Moldova", "Monaco", "Montenegro", "Netherlands",
-                      "North Macedonia", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino", "Serbia",
-                      "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "United Kingdom"]
